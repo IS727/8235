0f11e5a6808029004be18656ec74cbe55af9f82b
Ismael Gbian <ismael.gbian@gmail.com>
2020-09-18 18:42:12 -0400

    feat: added smooth turns

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -10,9 +10,7 @@ void ASDTAIController::Tick(float deltaTime)
     if (pawn)
     {
         UpdateSpeed(pawn, deltaTime);
-
-        if (DetectWordInFrontOfPawn(pawn))
-            pawn->AddActorWorldRotation(FRotator(0.0f, 5.0f, 0.0f));
+        HandleObstacles(pawn, deltaTime);
     }
 }
 
@@ -27,18 +25,65 @@ void ASDTAIController::UpdateSpeed(APawn* const pawn, float deltaTime)
     pawn->AddMovementInput(dir, speed);
 }
 
-bool ASDTAIController::DetectWordInFrontOfPawn(APawn* const pawn) const
+void ASDTAIController::HandleObstacles(APawn* const pawn, float deltaTime)
 {
-    const float lineTraceDistance = 300.0f;
+    const bool newWall = DetectWallInFrontOfPawn(pawn) && !turning;
+    AvoidWall(pawn, deltaTime, newWall, true);
+}
+
+/*
+ * Avoid a wall with a 90 degrees turn. Turning rate is around 45 degrees per 0.25 seconds
+ * 
+ * @param init If "true", initializes a new turn
+ * @param right If "true", turns to the right. Otherwise, turns to the left
+ */
+void ASDTAIController::AvoidWall(APawn* const pawn, float deltaTime, bool init, bool right)
+{
+    const float turnMaxDegree = 90.0f;
+    const float turnTime = 0.5f;
+    const float direction = right ? 1.0 : -1.0;
+    const float reachedHeading = FVector::Parallel(pawn->GetActorForwardVector(), newHeading);
+
+    if (init)
+    {
+        turning = true;
+        turnDeltaTime = 0.0f;
+        turnDegree = 0.0f;
+    }
+    else
+    {
+        turning = turning? !reachedHeading : turning;
+        if (turning)
+        {
+            // slow down for the turn
+            speed = 0.3;
+
+            // compute degree
+            const float expectedDegree = turnDeltaTime * (turnMaxDegree / turnTime);
+            const float degree = abs(expectedDegree - turnDegree);
+            turnDeltaTime += deltaTime;
+            turnDegree += degree;
+
+            // turn
+            pawn->AddActorWorldRotation(FRotator(0.0f, direction * degree, 0.0f));
+        }
+    }
+}
+
+bool ASDTAIController::DetectWallInFrontOfPawn(APawn* const pawn)
+{
+    const float lineTraceDistance = 150.0f;
     const FVector start(pawn->GetActorLocation());
     const FRotator pawnRotation(pawn->GetActorRotation());
     const FVector end(start + (pawnRotation.Vector() * lineTraceDistance));
 
     FHitResult hitResult;
-    return GetWorld()->LineTraceSingleByObjectType(
+    const bool wallDetected = GetWorld()->LineTraceSingleByObjectType(
         hitResult,
         start,
         end,
         ECC_WorldStatic
     );
+    if (!turning && wallDetected) newHeading = FRotator(0.0f, -90.0f, 0.0f).RotateVector(hitResult.ImpactNormal.GetSafeNormal());
+    return wallDetected;
 }
--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -17,11 +17,20 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 public:
     virtual void Tick(float deltaTime) override;
 
+    // ACCELERATION AND SPEED
     float acceleration = 0.3;
     float speed = 0;
     const float speedLimit = 0.6;
 
+    // TURN
+    bool turning = false;
+    float turnDeltaTime = 0.0f;
+    float turnDegree = 0.0f;
+    FVector newHeading;
+
 private:
     virtual void UpdateSpeed(APawn* const pawn, float deltaTime);
-    virtual bool DetectWordInFrontOfPawn(APawn* const pawn) const;
+    virtual void HandleObstacles(APawn* const pawn, float deltaTime);
+    virtual void AvoidWall(APawn* const pawn, float deltaTime, bool init, bool right);
+    virtual bool DetectWallInFrontOfPawn(APawn* const pawn);
 };
